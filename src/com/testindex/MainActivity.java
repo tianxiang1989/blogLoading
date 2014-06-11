6b5590fe5dd9ac030f8f9e5c0d8fa71208f1b78a
tianxiang1989 <qimingbuyi@sina.com>
2014-06-11 16:25:54 +0800

    rename pic

--- src/com/testindex/MainActivity.java
+++ src/com/testindex/MainActivity.java
@@ -1,27 +1,23 @@
 package com.testindex;
 
-import android.R.color;
 import android.app.Activity;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
-	/** Called when the activity is first created. */
+	
 	private int count = 5;
 	private int[] imgIDs = { R.id.widget29, R.id.widget30, R.id.widget31,
 			R.id.widget32, R.id.widget33 };
-	private int INDEX_SELECTED = 0;
+//	private int INDEX_SELECTED = 0;
 	private final int EDIT_TYPE_SELECTED = 1; // 选中的
 	private final int EDIT_TYPE_NO_SELECTED = 2; // 未选中的
 
+	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -40,17 +36,17 @@ public class MainActivity extends Activity {
 //		});
 	}
 
+	/**画loading圈的Handler*/
 	public Handler myHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
 			Log.i("Test", "---" + msg.arg1);
 			switch (msg.what) {
 			case EDIT_TYPE_SELECTED:
-				((ImageView) findViewById(msg.arg1)).setBackgroundResource(R.drawable.progress_go_small);
+				((ImageView) findViewById(msg.arg1)).setBackgroundResource(R.drawable.loginLoadingYes);
 				break;
 			case EDIT_TYPE_NO_SELECTED:
 				((ImageView) findViewById(msg.arg1)).setBackgroundResource(R.drawable.progress_bg_small);
-//				((ImageView) findViewById(msg.arg1)).setBackgroundResource(color.white);
 				break;
 			}
 		}
